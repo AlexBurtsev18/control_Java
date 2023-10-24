373330ec584b99da44870175aac661d5aa923325
«Alex18» <alexburtsev18@yandex.ru>
2023-10-24 10:44:24 +0300

    add func CheckPercent

--- Main.java
+++ Main.java
@@ -8,7 +8,31 @@ public class Main {
 
         String confirm = scanner.nextLine();
         if (confirm.equals('y')) {
+            int tempPercent;
+            int tempId = 1;
+            int remainder = 100;
+            Toy[] toys = new Toy[10];
 
+            while (remainder > 0) {
+                toys[tempId] = new Toy();
+                System.out.printf("Введите название %d-й игрушки: ", tempId);
+                String nameToy = scanner.nextLine();
+                System.out.print("Введите вероятность выпадения игрушки (от 1 до 100): ");
+                tempPercent = Integer.parseInt(scanner.nextLine());
+
+                toys[tempId].name = nameToy;
+                toys[tempId].id = tempId;
+                toys[tempId].weight = CheckPercent(tempPercent, remainder) / 10;
+
+            }
         }
+    }
+    static int CheckPercent(int percent, int remPercent){
+        if ((remPercent - percent) < 0) {
+            int toMach = remPercent - percent;
+            int newPercent = percent + toMach;
+            return newPercent;
+        }
+        return percent;
     }
 }
